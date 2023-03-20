359196b893ca87a7d9d4a4ebdc57d58900379adb
Cedric Hounnou <drico@Cedrics-MacBook-Air.local>
2023-03-20 16:20:26 +0100

    route to generate text is ready

--- controllers/openai.controller.js
+++ controllers/openai.controller.js
@@ -1,7 +1,8 @@
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-    // apiKey: process.env.API_KEY,
-  apiKey: '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',
+    // apiKey: process.env.API_KEY,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
+  apiKey: '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',
+  // apiKey: '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',
   // apiKey: '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'
   });
 const openai = new OpenAIApi(configuration);
